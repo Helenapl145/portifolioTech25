68bed56a27e442182c32268ef2fd03d21557d2fb
Helena <helenapl145@gmail.com>
2025-06-06 22:02:09 -0300

    add files

---
+++ main.js
@@ -0,0 +1,66 @@
+const hiddenImgs = document.querySelectorAll('.hidden-images div img');
+const hiddenLinks = document.querySelectorAll('.hidden-images div a'); 
+
+const total = hiddenImgs.length;
+let index = 0;
+
+const imgLeft = document.getElementById('left');
+const imgCenter = document.getElementById('center');
+const imgRight = document.getElementById('right');
+const centerLink = document.getElementById('centerLink'); 
+
+let autoSlideInterval = null;
+let userInteractedTimeout = null;
+
+function updateCarousel() {
+  const leftIndex = (index - 1 + total) % total;
+  const rightIndex = (index + 1) % total;
+
+  imgLeft.src = hiddenImgs[leftIndex].src;
+  imgCenter.src = hiddenImgs[index].src;
+  imgRight.src = hiddenImgs[rightIndex].src;
+
+  centerLink.href = hiddenLinks[index].href;
+}
+
+function nextSlide() {
+  index = (index - 1 + total) % total;
+  updateCarousel();
+}
+
+function prevSlide() {
+  index = (index + 1) % total;
+  updateCarousel();
+}
+
+function startAutoSlide() {
+  if (autoSlideInterval) clearInterval(autoSlideInterval);
+  autoSlideInterval = setInterval(prevSlide, 4000);
+}
+
+function userInteraction() {
+  if (autoSlideInterval) {
+    clearInterval(autoSlideInterval);
+    autoSlideInterval = null;
+  }
+  if (userInteractedTimeout) {
+    clearTimeout(userInteractedTimeout);
+  }
+  userInteractedTimeout = setTimeout(() => {
+    startAutoSlide();
+  }, 5000);
+}
+
+document.getElementById('nextBtn').addEventListener('click', () => {
+  nextSlide();
+  userInteraction();
+});
+
+document.getElementById('prevBtn').addEventListener('click', () => {
+  prevSlide();
+  userInteraction();
+});
+
+
+updateCarousel();
+startAutoSlide();
